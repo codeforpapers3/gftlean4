36d45cfa58e38f6e71ab9d0cc8afb6f824f7c45a
codeforpapers3 <153856069+codeforpapers3@users.noreply.github.com>
2026-08-12 08:22:59 +0500

    Add files via upload

---
+++ GFTLean4.lean
@@ -0,0 +1,20 @@
+/-
+Copyright (c) 2026 Adnan Rashid.
+School of Electrical Engineering and Computer Science (SEECS)
+National University of Sciences and Technology (NUST)
+Islamabad, Pakistan.
+
+Lean 4 formalization accompanying the SBMF 2026 paper:
+"Formal Semantics and Machine-Checked Metatheory for
+General Fault Trees in Lean 4"
+-/
+
+import GFTLean4.GeneralSyntax
+import GFTLean4.GeneralStructural
+import GFTLean4.GeneralProperties
+import GFTLean4.GeneralSemantics
+import GFTLean4.GeneralBooleanAlgebra
+import GFTLean4.GeneralMonotonicity
+import GFTLean4.GeneralPositive
+import GFTLean4.GeneralEquivalence
+import GFTLean4.SolarArrayCaseStudy
